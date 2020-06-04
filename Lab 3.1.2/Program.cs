fe45edf0d91a2c387398cb35761b1ff2adf9a081
Роман Данилевич <dwordchanel@gmail.com>
2020-06-05 00:36:05 +0300

    Add project files.

---
+++ Lab 3.1.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab_3._1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ввести строку : ");
+            string str = Console.ReadLine();
+            string[] split = str.Split(new char[] { ' ', ',', ':', ';', '-', '.' }, StringSplitOptions.RemoveEmptyEntries);
+            int minLength = Int32.MaxValue;
+            string smallestWord = String.Empty;
+            for (int i = 0; i < split.Length; i++)
+            {
+                if (split[i].Length < minLength)
+                {
+                    minLength = split[i].Length;
+                    smallestWord = split[i];
+                    if (minLength == 1)
+                        break;
+                }
+            }
+            Console.WriteLine(smallestWord);
+        }
+    }
+}
